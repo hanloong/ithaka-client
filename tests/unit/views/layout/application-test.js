552167615c31564f80e56394b3024e572950e47c
Han Loong Liauw <hanloongliauw@gmail.com>
2014-12-24 17:02:16 +1100

    added http proxy

---
+++ tests/unit/views/layout/application-test.js
@@ -0,0 +1,12 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('view:layout/application', 'LayoutApplicationView');
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var view = this.subject();
+  ok(view);
+});
