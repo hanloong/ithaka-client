b6c73067d38c8c38551e1f553339bdddcd5a2502
Han Loong Liauw <hanloongliauw@gmail.com>
2014-12-26 16:03:52 +1100

    wip

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,13 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 export default Ember.Route.extend(ApplicationRouteMixin, {
   beforeModel: function() {
     return this.csrf.fetchToken();
+  },
+  actions: {
+    sessionAuthenticationSucceeded: function() {
+      console.log('sessionAuthenticationSucceeded');
+    },
+    sessionInvalidationSucceeded: function() {
+      console.log('sessionInvalidationSucceeded');
+    }
   }
 });
