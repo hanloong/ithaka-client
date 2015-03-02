56f33a08caac91c580f715b123c4ff28d0ac5851
Han Loong Liauw <hanloongliauw@gmail.com>
2015-03-02 22:57:59 +1100

    trying to get comments working

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   comment:              DS.attr('string'),
+  anonymous:            DS.attr('boolean'),
   user:                 DS.belongsTo('user'),
   idea:                 DS.belongsTo('idea')
 });
--- app/routes/idea/index.js
+++ app/routes/idea/index.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var idea = this.modelFor('idea');
-    console.log(idea);
-    console.log(idea.get('comments'));
     return Ember.RSVP.hash({
       idea: idea,
       comments: idea.get('comments')
