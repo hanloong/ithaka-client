82ef8d4cc0068a6ef9248e9e28e916a190378446
Han Loong Liauw <hanloongliauw@gmail.com>
2015-03-02 21:42:02 +1100

    removed controller.model

--- app/routes/idea/index.js
+++ app/routes/idea/index.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var idea = this.modelFor('idea');
+    console.log(idea);
+    console.log(idea.get('comments'));
     return Ember.RSVP.hash({
       idea: idea,
       comments: idea.get('comments')
